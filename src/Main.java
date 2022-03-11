d43eac3a4b0d85de6c0512ff13955723efe470cf
D <dannyc.88ninja@gmail.com>
2022-03-11 13:02:04 -0500

    saved changes

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,18 @@ public class Main {
 //        Encryptor encryptor = new Encryptor(3, 2);
 //        String x = encryptor.decryptMessage("Ti hsiscn ofietdnia!Bl e nto helo okotAu!A");
 //        System.out.println(x);
-
+//
 //        Encryptor en = new Encryptor(5, 7);
 //        en.fillBlock("Reddit is the answer to everything.");
 //        String f = en.encryptBlock();
 //        System.out.println(f);
 
-        Encryptor e = new Encryptor (5, 7);
-        String x = e.decryptMessage("Riatyesnotd s hdtweiihevntereg   r.");
-        System.out.println(x);
+//        Encryptor e = new Encryptor(5, 7);
+//        String r = e.decryptMessage("Riatyesnotd s hdtweiihevntereg   r.");
+//        System.out.println(r);
+
+        Encryptor c = new Encryptor(3, 5);
+        String t = c.decryptMessage("I isMlnrl’.etMr BAtEPhS eTC  SAcA hAteAerAa?A");
+        System.out.println(t);
     }
 }
